882c4d6199d45b767bddbbdce5b046b49d96061a
SamWilk <samwilk18@yahoo.com>
2022-10-10 20:34:31 -0400

    Creating book controller

---
+++ BookKeepingBackEnd/BookKeepingBackEnd/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookKeepingBackEnd.Controllers
+{
+    public class BookController : ControllerBase
+    {
+        public BookController()
+        {
+        }
+
+        [HttpGet]
+        public async Task GetBooks()
+        {
+
+        }
+
+    }
+}
+
+s
